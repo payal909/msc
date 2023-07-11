1dc79f4d69dc6e4ccfadc8e97c9771fdc4eba13c
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-07-11 21:00:00 +0000

    updated ui

--- bcar.py
+++ bcar.py
@@ -34,28 +34,38 @@ embeddings = OpenAIEmbeddings(model="text-embedding-ada-002",chunk_size =1)
 
 bank_names = {"BMO":"bmo_ar2022 (2)_index","NBC":"NATIONAL BANK OF CANADA_ 2022 Annual Report (1)_index"}
 
-l,r = st.columns([2,1])
-with l:
-    bank_name = st.selectbox(label="Institute",options=bank_names)
-with r:
-    analyse_button = st.button("Analyse",use_container_width=True)
-# template = """
-# You are virtual assistant of OSFI.
-# Use the following  context (delimited by <ctx></ctx>), and the chat history (delimited by <hs></hs>) to answer the question:
-# ------
-# <ctx>
-# {context}
-# </ctx>
-# ------
-# <hs>
-# {history}
-# </hs>
-# ------
-# {question}
-# Answer:
-# """
-# prompt = PromptTemplate(input_variables=["history", "context", "question"],template=template)
+bank_name = st.selectbox(label="Institute",options=bank_names)
+analyse_button = st.button("Analyse",use_container_width=True)
 
+bank_db = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name=bank_names[bank_name])
+
+template = """
+You are virtual assistant of OSFI.
+Use the following  context (delimited by <ctx></ctx>), and the chat history (delimited by <hs></hs>) to answer the question:
+------
+<ctx>
+{context}
+</ctx>
+------
+<hs>
+{history}
+</hs>
+------
+{question}
+Answer:
+"""
+prompt = PromptTemplate(input_variables=["history", "context", "question"],template=template)
+
+agent = RetrievalQA.from_chain_type(llm = llm,
+    chain_type='stuff', # 'stuff', 'map_reduce', 'refine', 'map_rerank'
+    retriever=bank_db.as_retriever(),
+    verbose=False,
+    chain_type_kwargs={
+    "verbose":True,
+    "prompt": prompt,
+    "memory": ConversationBufferMemory(
+        input_key="question"),
+})
 
 # bcar_retriever = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='Basel Capital Adequacy Reporting (BCAR) 2023 (2)_index')
 # bmo_retriver = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='bmo_ar2022 (2)_index')
@@ -83,56 +93,22 @@ with r:
 #         input_key="question"),
 # })
 
-# # st.title("BMO Chatbot")
-
-# # if 'something' not in st.session_state:
-# #     user_input = ''
-
-# # def submit():
-# #     user_input = st.session_state.widget
-# #     st.session_state.widget = ''
-
-# # if 'generated' not in st.session_state:
-# #     st.session_state['generated'] = []
-# # ## past stores User's questions
-# # if 'past' not in st.session_state:
-# #     st.session_state['past'] = []
-# # messages = st.container()
-# # user_input = st.text_input("Query", key="widget", on_change=submit)
-# # relevent_docs = st.expander("Relevent Docs", expanded=False)
-# # if user_input:
-# #     output = agent.run(user_input)
-# #     with relevent_docs:
-# #         st.write("\n\n\n",bcar_retriever.as_retriever().get_relevant_documents(user_input),"\n\n\n")
-# #     st.session_state.past.append(user_input)
-# #     st.session_state.generated.append(output)
-# # if 'generated' in st.session_state:
-# #     with messages:
-# #         for i in range(len(st.session_state['generated'])):
-# #             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user',avatar_style="initials",seed="U")
-# #             message(st.session_state["generated"][i], key=str(i),avatar_style="initials",seed="A")
-
-# # agent.run("Which reports bank BMO has to send to OSFI for BCAR Credit Risk?")
-
-# # print(bcar_retriever.as_retriever().get_relevant_documents("what is fiscal year end of BMO?"))
 
 # st.title("BMO Chatbot")
 
-# # if 'something' not in st.session_state:
-# #     user_input = ''
+if 'transcript' not in st.session_state:
+    st.session_state['transcript'] = []
 
-# # def submit():
-# #     user_input = st.session_state.widget
-# #     st.session_state.widget = ''
+if 'input_disabled' not in st.session_state:
+    st.session_state['input_disabled'] = True
 
-# if 'generated' not in st.session_state:
-#     st.session_state['generated'] = []
-# ## past stores User's questions
-# if 'past' not in st.session_state:
-#     st.session_state['past'] = []
-# messages = st.container()
-# user_input = st.chat_input("Query")
-# relevent_docs = st.expander("Relevent Docs", expanded=False)
+
+messages = st.container()
+user_input = st.chat_input("Query",disabled=st.session_state['input_disabled'])
+
+st.chat_message("user",avatar=":computer:").write("System Message")
+
+
 # if user_input:
 #     output = agent.run(user_input)
 #     # with relevent_docs:
