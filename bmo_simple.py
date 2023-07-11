0da3715770a2a4005882f5fd24e8e3e5cc9c3753
payal909 <129751459+payal909@users.noreply.github.com>
2023-07-11 09:11:20 +0000

    updaed imports

--- bmo_simple.py
+++ bmo_simple.py
@@ -1,17 +1,11 @@
 import os
-# from dotenv import load_dotenv, find_dotenv
-from langchain.chains import RetrievalQA
+# from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
-from langchain.llms import AzureOpenAI
-from langchain.document_loaders import DirectoryLoader,PyPDFLoader
-from langchain.document_loaders import UnstructuredExcelLoader
-from langchain.vectorstores import DocArrayInMemorySearch
-from langchain.memory import ConversationBufferMemory
-from IPython.display import display, Markdown
-import pandas as pd
-import gradio as gr
-import random
-import time
+# from langchain.document_loaders import DirectoryLoader,PyPDFLoader
+# from langchain.document_loaders import UnstructuredExcelLoader
+# from langchain.vectorstores import DocArrayInMemorySearch
+# from langchain.memory import ConversationBufferMemory
+
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain import PromptTemplate
