a85c649041803e2e05aade2005d558f7dd3a8936
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-07-11 20:22:59 +0000

    updated ui

--- bcar.py
+++ bcar.py
@@ -1,88 +1,122 @@
 import os
+# from dotenv import load_dotenv, find_dotenv
+# from langchain.llms import GooglePalm
+from langchain.vectorstores import FAISS
+# from langchain.embeddings import GooglePalmEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+# from langchain.llms import AzureOpenAI
 # from langchain.document_loaders import DirectoryLoader,PyPDFLoader
 # from langchain.document_loaders import UnstructuredExcelLoader
 # from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.memory import ConversationBufferMemory
-
+# from IPython.display import display, Markdown
+# import pandas as pd
+# import gradio as gr
 # from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain import PromptTemplate
-from langchain.vectorstores import Chroma
+# from langchain.vectorstores import Chroma
+# from langchain.agents.tools import Tool
+# from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
+# from langchain import OpenAI, VectorDBQA
+# from langchain.chains.router import MultiRetrievalQAChain
+import streamlit as st
+from streamlit_chat import message
+# from langchain.document_loaders import UnstructuredPDFLoader
+# _ = load_dotenv(find_dotenv())
 
-os.environ["OPENAI_API_KEY"] = "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"
+bank_names = {"BMO":"bmo_ar2022 (2)_index","NBC":"NATIONAL BANK OF CANADA_ 2022 Annual Report (1)_index"}
+
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.1)
-
-
-template = """
-You are virtual assistant of OSFI.
-Use the following  context (delimited by <ctx></ctx>), and the chat history (delimited by <hs></hs>) to answer the question:
-------
-<ctx>
-{context}
-</ctx>
-------
-<hs>
-{history}
-</hs>
-------
-{question}
-Answer:
-"""
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002",chunk_size =1)
 
-
-db_bcar = Chroma(embedding_function=embeddings,persist_directory="./zip_bmo emb/BCAR_Embedding")
-db_bmo = Chroma(embedding_function=embeddings,persist_directory="./zip_bmo emb/BMO_FULL_EMBEDDING")
-db_credirirb = Chroma(embedding_function=embeddings,persist_directory="./zip_bmo emb/IRB")
-db_creditstd = Chroma(embedding_function=embeddings,persist_directory="./zip_bmo emb/credit_risk_standartize")
-db_smsb = Chroma(embedding_function=embeddings,persist_directory="./zip_bmo emb/SMSB_EMBEDDING")
-db_ncb = Chroma(embedding_function=embeddings,persist_directory="./zip_bmo emb/NBC_Embedding")
-
-dbs = [db_bmo ,db_credirirb ,db_creditstd ,db_smsb ,db_ncb]
-
-db_bcar._collection.add(
-    embeddings=db_bmo.get()["embeddings"],
-    metadatas=db_bmo.get()["metadatas"],
-    documents=db_bmo.get()["documents"],
-    ids=db_bmo.get()["ids"])
-
-# for db in dbs[0:1]:
-#   db_bcar._collection.add(
-#     embeddings=db.get()["embeddings"],
-#     metadatas=db.get()["metadatas"],
-#     documents=db.get()["documents"],
-#     ids=db.get()["ids"])
-
-prompt = PromptTemplate(input_variables=["history", "context", "question"],template=template)
-
-retriever = db_bcar.as_retriever()
+# template = """
+# You are virtual assistant of OSFI.
+# Use the following  context (delimited by <ctx></ctx>), and the chat history (delimited by <hs></hs>) to answer the question:
+# ------
+# <ctx>
+# {context}
+# </ctx>
+# ------
+# <hs>
+# {history}
+# </hs>
+# ------
+# {question}
+# Answer:
+# """
+# prompt = PromptTemplate(input_variables=["history", "context", "question"],template=template)
 
 
-qa = RetrievalQA.from_chain_type(llm = llm,
-    chain_type='stuff', # 'stuff', 'map_reduce', 'refine', 'map_rerank'
-    retriever=retriever,
-    verbose=False,
-    chain_type_kwargs={
-    "verbose":True,
-    "prompt": prompt,
-    "memory": ConversationBufferMemory(
-        memory_key="history",
-        input_key="question"),
-})
+# bcar_retriever = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='Basel Capital Adequacy Reporting (BCAR) 2023 (2)_index')
+# bmo_retriver = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='bmo_ar2022 (2)_index')
+# creditirb_retriever = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='Capital Adequacy Requirements (CAR) Chapter 5 Credit Risk Internal Ratings Based Approach_index')
+# creditstd_retriever = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='Capital Adequacy Requirements (CAR) Chapter 4  Credit Risk Standardized Approach_index')
+# nbc_retriever = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='NATIONAL BANK OF CANADA_ 2022 Annual Report (1)_index')
+# smsb_retriever = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name='SMSB (1)_index')
+
+# indices = [bcar_retriever,bmo_retriver,creditirb_retriever,creditstd_retriever,nbc_retriever,smsb_retriever]
+
+# for index in indices[1:]:
+#     indices[0].merge_from(index)
 
 
-print(qa.run("Hi"))
+
+# agent = RetrievalQA.from_chain_type(llm = llm,
+#     chain_type='stuff', # 'stuff', 'map_reduce', 'refine', 'map_rerank'
+#     retriever=bcar_retriever.as_retriever(),
+#     verbose=False,
+#     chain_type_kwargs={
+#     "verbose":True,
+#     "prompt": prompt,
+#     "memory": ConversationBufferMemory(
+#         memory_key="history",
+#         input_key="question"),
+# })
+
+# # st.title("BMO Chatbot")
+
+# # if 'something' not in st.session_state:
+# #     user_input = ''
+
+# # def submit():
+# #     user_input = st.session_state.widget
+# #     st.session_state.widget = ''
+
+# # if 'generated' not in st.session_state:
+# #     st.session_state['generated'] = []
+# # ## past stores User's questions
+# # if 'past' not in st.session_state:
+# #     st.session_state['past'] = []
+# # messages = st.container()
+# # user_input = st.text_input("Query", key="widget", on_change=submit)
+# # relevent_docs = st.expander("Relevent Docs", expanded=False)
+# # if user_input:
+# #     output = agent.run(user_input)
+# #     with relevent_docs:
+# #         st.write("\n\n\n",bcar_retriever.as_retriever().get_relevant_documents(user_input),"\n\n\n")
+# #     st.session_state.past.append(user_input)
+# #     st.session_state.generated.append(output)
+# # if 'generated' in st.session_state:
+# #     with messages:
+# #         for i in range(len(st.session_state['generated'])):
+# #             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user',avatar_style="initials",seed="U")
+# #             message(st.session_state["generated"][i], key=str(i),avatar_style="initials",seed="A")
+
+# # agent.run("Which reports bank BMO has to send to OSFI for BCAR Credit Risk?")
+
+# # print(bcar_retriever.as_retriever().get_relevant_documents("what is fiscal year end of BMO?"))
 
 # st.title("BMO Chatbot")
 
-# if 'something' not in st.session_state:
-#     st.session_state.something = ''
+# # if 'something' not in st.session_state:
+# #     user_input = ''
 
-# def submit():
-#     st.session_state.something = st.session_state.widget
-#     st.session_state.widget = ''
+# # def submit():
+# #     user_input = st.session_state.widget
+# #     st.session_state.widget = ''
 
 # if 'generated' not in st.session_state:
 #     st.session_state['generated'] = []
@@ -90,13 +124,16 @@ print(qa.run("Hi"))
 # if 'past' not in st.session_state:
 #     st.session_state['past'] = []
 # messages = st.container()
-# user_input = st.text_input("Query", key="widget", on_change=submit)
-# if st.session_state.something:
-#     output = qa.run(st.session_state.something)
-#     st.session_state.past.append(st.session_state.something)
+# user_input = st.chat_input("Query")
+# relevent_docs = st.expander("Relevent Docs", expanded=False)
+# if user_input:
+#     output = agent.run(user_input)
+#     # with relevent_docs:
+#     #     st.write("\n\n\n",bcar_retriever.as_retriever().get_relevant_documents(user_input),"\n\n\n")
+#     st.session_state.past.append(user_input)
 #     st.session_state.generated.append(output)
 # if 'generated' in st.session_state:
 #     with messages:
 #         for i in range(len(st.session_state['generated'])):
-#             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user',avatar_style="initials",seed="U")
-            # message(st.session_state["generated"][i], key=str(i),avatar_style="initials",seed="B")
+#             st.chat_message("user").write(st.session_state['past'][i])
+#             st.chat_message("assistant").write(st.session_state["generated"][i])
