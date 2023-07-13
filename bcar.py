cc35c2cf3159546e8ed4051efb0185f9cc79feb5
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-07-13 13:10:57 +0000

    updated temprature

--- bcar.py
+++ bcar.py
@@ -74,7 +74,7 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 os.environ["OPENAI_API_BASE"] = "https://testavinx.openai.azure.com/"
 
 # llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.1)
-llm = AzureChatOpenAI(deployment_name="gpt-35-turbo",model_name="gpt-35-turbo")
+llm = AzureChatOpenAI(deployment_name="gpt-35-turbo",model_name="gpt-35-turbo",temperature=0)
 embeddings = OpenAIEmbeddings(deployment="embedding1",
 model="text-embedding-ada-002",
 openai_api_base="https://testavinx.openai.azure.com/",
