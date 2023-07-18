06a5b6352bd660cac3235009fe04802ef9cbe6db
KTerhuja <132392099+KTerhuja@users.noreply.github.com>
2023-07-18 06:50:09 +0000

    fixed ui

--- main.py
+++ main.py
@@ -93,9 +93,9 @@ if user_input:
     session.transcript.append(["assistant",response])
 
 if len(session.transcript)>0:
-    with st.chat_message("assistant"):
-        st.write(session.transcript[0])
-        st.dataframe(session.transcript[1])
-    for message in session.transcript[2:]:
+    # with st.chat_message("assistant"):
+    #     st.write(session.transcript[0])
+        # st.dataframe(session.transcript[1])
+    for message in session.transcript:
         st.chat_message(message[0]).write(message[1])
 
